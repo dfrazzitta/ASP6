f2bdb282e1ae967e6f1306a071b28585d2ec681d
dfrazzitta <dfrazzitta@pctrax.com>
2022-02-02 10:50:11 -0500

    init

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP6.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Message { get; set; } = "Initial Request";
+
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            Message = "Form Posted";
+        }
+        public void OnPostDelete()
+        {
+            Message = "Delete handler fired";
+        }
+        public void OnPostEdit(int id)
+        {
+            Message = "Edit handler fired";
+        }
+        public void OnPostView(int id)
+        {
+            Message = "View handler fired";
+        }
+    }
+}
